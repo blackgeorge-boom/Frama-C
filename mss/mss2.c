0a2900c8f02fa32953c12c9f89298fcd360d1d29
Takuo Watanabe <wtakuo@me.com>
2011-06-09 16:31:18 +0900

    Added mss2.c.

---
+++ mss/mss2.c
@@ -0,0 +1,58 @@
+/*
+ * Maximum segment sum: O(n^2) implementation
+ * Takuo Watanabe (Tokyo Institute of Technology)
+ *
+ * To verify this file using Frama-C:
+ *   frama-c -jessie mss.c
+ *   frama-c -jessie -jessie-atp=ergo mss.c
+ *
+ */
+
+#ifndef NO_JESSIE_PRAGMAS
+#pragma JessieIntegerModel(math)
+#endif
+
+#include "max.h"
+#include "mss.h"
+
+int mss (int a[], int n) {
+    int s = a[0];
+    //@ ghost int p = 0, q = 0;
+    /*@ loop invariant 0 <= i <= n;
+      @ loop invariant
+      @      \forall integer k, l; 0 <= k < i && k <= l < n 
+      @               ==> s >= ssum(a, k, l);
+      @ loop invariant 0 <= p <= q < n;
+      @ loop invariant s == ssum(a, p, q);
+      @ loop variant n - i;
+      @*/
+    for (int i = 0; i < n; i++) {
+        int t = 0;
+        /*@ loop invariant i <= j <= n;
+          @ loop invariant 
+          @      \forall integer k, l; 0 <= k < i && k <= l < n 
+          @               ==> s >= ssum(a, k, l);
+          @ loop invariant
+          @      \forall integer k; i <= k < j
+          @              ==> s >= ssum(a, i, k);
+          @ loop invariant t == ssum(a, i, j - 1);
+          @ loop invariant 0 <= p <= q < n;
+          @ loop invariant s == ssum(a, p, q);
+          @ loop variant n - j;
+          @*/
+        for (int j = i; j < n; j++) {
+            t = t + a[j];
+            //@ ghost if (s < t) { p = i; q = j; }
+            s = max(s, t);
+        }
+    }
+    return s;
+}
+
+/*
+0:0 0:1 0:2 0:3 0:4
+    1:1 1:2 1:3 1:4
+        2:2 2:3 2:4
+            3:3 3:4
+                4:4
+*/
